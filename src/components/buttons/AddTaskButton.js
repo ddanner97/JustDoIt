91bfcf436a835f6e9843a56bcdd1cd9a7d571292
Damian <ddanner97@gmail.com>
2022-09-26 12:35:21 -0500

    Fix add task button

--- src/components/buttons/AddTaskButton.js
+++ src/components/buttons/AddTaskButton.js
@@ -2,9 +2,9 @@ import React from 'react'
 
 function AddTaskButton(props) {
     return (
-        <div className="flex flex-row mx-5">
+        <div className="flex flex-row px-5 py-5">
             <div className="grow"></div>
-            <button onClick={() => props.viewForm ? props.setViewForm(false) : props.setViewForm(true)} ><i className="fa-regular fa-plus bg-primary text-1xl text-white p-6 rounded-full mt-5 mb-8"></i></button>
+            <button onClick={() => props.viewForm ? props.setViewForm(false) : props.setViewForm(true)} ><i className="fa-regular fa-plus bg-primary text-1xl text-white p-6 rounded-full"></i></button>
         </div>
     )
 }
